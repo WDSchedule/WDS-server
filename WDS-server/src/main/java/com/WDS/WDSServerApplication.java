6623177b1ad7665b6899b3eb74a65eeb2196b705
wowDespacito <wangzhi_wow@163.com>
2024-09-03 23:56:14 +0800

    创建springboot工程

---
+++ WDS-server/src/main/java/com/WDS/WDSServerApplication.java
@@ -0,0 +1,17 @@
+package com.WDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class WDSServerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(WDSServerApplication.class, args);
+    }
+}
