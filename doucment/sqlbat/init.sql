71e913cab50caea53f1893b2fcd64d161a02df10
Cooper Wang <91377716+WowDespacito@users.noreply.github.com>
2024-09-05 11:51:48 +0800

    新增账户表

--- doucment/sqlbat/init.sql
+++ doucment/sqlbat/init.sql
@@ -1,3 +1,11 @@
 CREATE USER 'WDS'@'localhost' IDENTIFIED BY '@WDS1024';
 CREATE DATABASE WDS CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 GRANT ALL PRIVILEGES ON WDS.* TO 'WDS'@'localhost';
+CREAte TABLE account (
+  account_no INT AUTO_INCREMENT
+  username VCHAR(30) NOT NULL,
+  password VCHAR(50) NOT NULL,
+  UNIQUE KEY unique_username(username),
+  INDEX index_username(username),
+  PRIMARY KEY(account_id)
+);
