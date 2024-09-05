b2703d687f345fd3a2608da42e6ef6dbc1565ace
wowDespacito <wangzhi_wow@163.com>
2024-09-05 23:34:00 +0800

    创建登录功能

--- doucment/sqlbat/init.sql
+++ doucment/sqlbat/init.sql
@@ -1,11 +1,17 @@
 CREATE USER 'WDS'@'localhost' IDENTIFIED BY '@WDS1024';
 CREATE DATABASE WDS CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 GRANT ALL PRIVILEGES ON WDS.* TO 'WDS'@'localhost';
-CREAte TABLE account (
-  account_no INT AUTO_INCREMENT
-  username VCHAR(30) NOT NULL,
-  password VCHAR(50) NOT NULL,
-  UNIQUE KEY unique_username(username),
-  INDEX index_username(username),
-  PRIMARY KEY(account_id)
+USE WDS;
+CREATE TABLE account
+(
+    account_no INT AUTO_INCREMENT,
+    username   VARCHAR(30) NOT NULL,
+    password   VARCHAR(50) NOT NULL,
+    create_time datetime,
+    update_time datetime,
+    UNIQUE KEY unique_username (username),
+    INDEX index_username (username),
+    INDEX index_id(account_no),
+    PRIMARY KEY (account_no)
 );
+ALTER TABLE account ADD nickname VARCHAR(20);
